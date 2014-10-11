1285c1557f79cf5e58b41e27a873308e78f92d6c
PabloSoligo2014 <psoligo@ing.unlam.edu.ar>
2014-10-10 23:02:40 -0300

    Esqueleto libreria

---
+++ EncuestaContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Text;
+using System.Data.SqlClient;
+using System.Collections;
+
+
+namespace EncuestaContextLib
+{
+    public class EncuestaContext
+    {
+        
+
+        Agregar metodo de login tambien!
+
+        public EncuestaContext(String ConnectionString) {
+           
+
+        }
+
+        public List<TipoEncuesta> ObtenerTiposDeEncuesta() {
+
+            
+        }
+
+        public bool ResponderPregunta(int IdPregunta, String Respuesta) {
+
+           
+        }
+
+
+        public TEncuesta NuevaEncuesta(int IdTipoEncuesta) {
+            
+        }
+
+
+        public int Salvar() {
+        
+        }
+    }
+}
